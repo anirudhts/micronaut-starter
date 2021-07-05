e5a6750d471b9d0e66f7146e0e530bbfe9a49ef3
Shiyas N <shiyas.nazar@thoughtworks.com>
2021-07-05 16:53:28 +0530

    Shiyas/Anirudh | Added customer object in account class

--- src/main/java/com/project/models/Account.java
+++ src/main/java/com/project/models/Account.java
@@ -12,10 +12,12 @@ public class Account {
   private Long accountId;
   private String accountType;
   private String branch;
+  private Customer customer;
 
   public Account(AccountEntity accountEntity) {
     this.accountId = accountEntity.getAccountId();
     this.accountType = accountEntity.getAccountType();
     this.branch = accountEntity.getBranch();
+    this.customer = new Customer(accountEntity.getCustomerEntity());
   }
 }
